02a8ae411dfbc9442799400d019c7a962d466c89
Mahudoom <66937573+easa2000@users.noreply.github.com>
2021-06-28 07:45:13 +0530

    Create GCD-of-two-numbers.c

---
+++ GCD-of-two-numbers.c
@@ -0,0 +1,31 @@
+// GCD of two numbers
+
+#include<stdio.h>
+void main()
+{
+	int num1,num2,dividend,divisor,remainder;
+	printf("\n Enter the first number: ");
+	scanf("%d",&num1);
+	
+	printf("\n Enter the second number: ");
+	scanf("%d",&num2);
+	
+	if(num1>num2)
+	{
+		dividend = num1;
+		divisor = num2;
+	}
+	else
+	{
+		dividend = num2;
+		divisor = num1;
+	}
+	while(divisor)
+	{
+		remainder = dividend % divisor;
+		dividend = divisor;
+		divisor = remainder;
+	}
+	
+	printf("\n GCD of %d and %d is = %d ",num1,num2,dividend);
+}
